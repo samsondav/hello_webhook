c9031929c745ea9aa143c364eecf26857b92d886
Sam Davies <sampdavies@gmail.com>
2017-04-29 11:30:59 +0100

    Add port configuration for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -1,3 +1,9 @@
 use Mix.Config
 
-config :hello_webhook, port: 80
+port =
+  case System.get_env("PORT") do
+    port when is_binary(port) -> String.to_integer(port)
+    nil -> 80 # default port
+  end
+
+config :hello_webhook, port: port
